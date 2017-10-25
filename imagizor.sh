48b4a5c6643fe6d2dcac2b9dc3b6fb77610557c6
Tobias.Turi <t.turi@jung.de>
2017-10-25 10:58:37 +0200

    close #4 USB Stick support

--- imagizor.sh
+++ imagizor.sh
@@ -4,7 +4,7 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
    
 set +u
 set -e
-set -x
+#set -x
 
 #echo -e "$0 Parameter: $*"
 
@@ -50,7 +50,7 @@ unpack_text() {      #Text for the unpack part
 help() {                    #Is a help text
     echo -e "invalid command"
     echo -e "Call: ./image_to_device.sh [-d, --download, -g, --gunzip] [Downloadlink, File to unpack]"
-    echo -e "Example: ./image_to_device.sh -d http://download.opensuse.org/distribution/leap/42.3/iso/openSUSE-Leap-42.3-DVD-x86_64.iso.sha256"
+    echo -e "Example: ./imagizor.sh -d http://download.opensuse.org/distribution/leap/42.3/iso/openSUSE-Leap-42.3-DVD-x86_64.iso.sha256"
     exit 
 }
 
@@ -64,7 +64,7 @@ help_for_less_Parameter () {     #Longer help text
     echo -e "Call: ./image_to_device.sh [-d, --download, -g, --gunzip] [Downloadlink, File to unpack]"
     echo -e "./image_to_device.sh                    -g      --gunzip                            File to unpack"
     echo -e "./image_to_device.sh                    -d      --download                          Downloadlink"
-    echo -e "Example: ./image_to_device.sh -d http://download.opensuse.org/distribution/leap/42.3/iso/openSUSE-Leap-42.3-DVD-x86_64.iso.sha256"
+    echo -e "Example: ./imagizor.sh -d http://download.opensuse.org/distribution/leap/42.3/iso/openSUSE-Leap-42.3-DVD-x86_64.iso.sha256"
     exit
 }
 
@@ -86,10 +86,29 @@ Find_Out_SD_Card () {     #Checked if the SD-Card exists
     done
 }
 
+Find_USB_stick_out () {  #Checked if the USb-Stick exists
+    Head_trace "Find out the USB-Stick"
+    Info_trace "Checked if the USB-Stick exists"
+    if ! [ -b /dev/sdb ]; then 
+        error_trace "USB-Stick is not available"
+        Help_trace "Please put a SD-Card in"
+        Help_trace "At least $FILESIZE are needed"
+    fi
+    while true; do 
+        sleep 1
+        declare SIZE=$(lsblk $USB_DEVICE 2>/dev/null | grep "sdb " | awk '{print $4}' )
+    if [ -b /dev/sdb ]; then
+        SIZE_trace "The USB-Stick is $SIZE big"
+        break
+    fi
+    done
+}
+
 Checked_SD-Card_and_FileSize () {      #Checked the Sd-Card Size and the Filesize
     Head_trace "Checking Size"
     Info_trace "Checked the Size of the SD-Card and the Image-File"
-    if [ $SIZE_WHOLE -lt $FILESIZE_WHOLE ]; then
+    declare SIZE_WHOLE=$(lsblk -b $SDCard_DEVICE 2>/dev/null | grep "mmcblk0 " | awk '{print $4}' )
+    if [ $SIZE_WHOLE -lt $FILESIZE_WHOLE ] >/dev/null 2>/dev/null; then
         error_trace "SD-Card has less memory space"
         Help_trace "Please put a new SD-Card in"
         Help_trace "Or provide more memory Space"
@@ -105,11 +124,39 @@ Checked_SD-Card_and_FileSize () {      #Checked the Sd-Card Size and the Filesiz
     done 
 }
 
+Checked_USB_Stick_and_FileSize () {      #Checked the USB-Stick Size and the Filesize
+    Head_trace "Checking Size"
+    Info_trace "Checked the Size of the USB-Stick and the Image-File"
+    declare SIZE_WHOLE=$(lsblk -b $USB_DEVICE 2>/dev/null | grep "sdb " | awk '{print $4}' )
+    if [ $SIZE_WHOLE -lt $FILESIZE_WHOLE ] >/dev/null 2>/dev/null; then
+        error_trace "USb-Stick has less memory space"
+        Help_trace "Please put a new USB-Stick in"
+        Help_trace "Or provide more memory Space"
+        Help_trace "At least $FILSIZE are needed"
+    fi
+    while true; do
+        sleep 1
+    declare SIZE_WHOLE=$(lsblk -b $USB_DEVICE 2>/dev/null | grep "sdb " | awk '{print $4}' )
+    if [ $SIZE_WHOLE -gt $FILESIZE_WHOLE ] >/dev/null 2>/dev/null; then
+        Correct_trace "USB-Stick is bigger then the Image-File "
+        break
+    fi
+    done 
+}
+
 CopySD () {             #Copy the File on the SD-Card
     Head_trace "Copy process"
     Info_trace "Copy the File on the SD-Card"
     declare BLOCKS=8000000
     sudo dd if=$FILENAME of=$SDCard_DEVICE bs=$BLOCKS count=$((FILESIZE_WHOLE))
+    sync
+}
+
+CopyUSB () {             #Copy the File on the USB-Stick
+    Head_trace "Copy process"
+    Info_trace "Copy the File on the SD-Card"
+    declare BLOCKS=8000000
+    sudo dd if=$FILENAME of=$USB_DEVICE bs=$BLOCKS count=$((FILESIZE_WHOLE))
     sync
 }
 
@@ -123,13 +170,23 @@ Copy_back() {           #Copy the File from the SD-Card back into an File
     sudo truncate -r $FILENAME verify.img
 }
 
+Copy_back_USB() {           #Copy the File from the USB-Stick back into an File
+    Head_trace "Verifying"
+    Info_trace "Copy the File from the USB-Stick back into an File"
+    declare -r BLOCKS_BACK=1000000
+    sudo dd if=$USB_DEVICE of=verify.img bs=$BLOCKS_BACK count=$((FILESIZE_WHOLE))
+    sync
+    Info_trace "Shortening the returned File in the Size from the original File"
+    sudo truncate -r $FILENAME verify.img
+}
+
 Filesize () {                       #Checked the Filesize
     Head_trace "Size checking"
     Info_trace "Checked the Filesize of the Image-File"
     SIZE_trace "Filesize of the Image-File: $FILESIZE" 
 }
 
-Hashwerte_Vergleichen() {   #Compares the hash values from the downloaded File and the returned File
+Compare_hash_values() {   #Compares the hash values from the downloaded File and the returned File
     Info_trace "Compare the hash values from the downloaded File and the returned File"
     declare MD5SUM=$(md5sum $FILENAME | cut -d" " -f1)
     declare MD5SUM_BACK=$(md5sum verify.img | cut -d" " -f1)
@@ -211,26 +268,49 @@ esac
 declare FILESIZE_WHOLE=$(stat -c %s $FILENAME)
 declare FILESIZE=$(du -h $FILENAME | awk '{print $1}') 
 declare SDCard_DEVICE=/dev/mmcblk0
+declare USB_DEVICE=/dev/sdb
 declare SIZE=""
 declare SIZE_WHOLE=""
 
-Find_Out_SD_Card
+read -p "Do you want to copy on the SD-Card or on the USB-Stick?:" answer
 
-declare SIZE_WHOLE=$(lsblk -b $SDCard_DEVICE | grep "mmcblk0 " | awk '{print $4}' )
+case "$answer" in
+    USB|USB-Stick|Usb-Stick|usb-stick|Usb|usb|u|U)
+        Find_USB_stick_out
+        
+        Checked_USB_Stick_and_FileSize
+        
+        Filesize
+        
+        CopyUSB
+        
+        Copy_back_USB
+        
+        Compare_hash_values
+        
+        Info_trace "Delete the returned File"
+        
+        delete_returned_file
+         
+        Correct_trace "You can remove the USB-Stick"
+        ;;
+    SD-Card|Sd-Card|sd-Card|sd-card|SD|Sd|sd|S|s)
+        Find_Out_SD_Card
 
-Checked_SD-Card_and_FileSize
+        Checked_SD-Card_and_FileSize
 
-Filesize
+        Filesize
 
-CopySD  
+        CopySD  
 
-Copy_back
+        Copy_back
 
-Hashwerte_Vergleichen
+        Compare_hash_values
 
-Info_trace "Delete the returned File"
+        Info_trace "Delete the returned File"
 
-delete_returned_file
+        delete_returned_file
 
-Correct_trace "You can remove the Sd-Card"
-
+        Correct_trace "You can remove the Sd-Card"
+        ;;
+esac
