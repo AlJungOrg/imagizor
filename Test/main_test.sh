e18dcc5524f286381b12eca28ec234b41296b2f4
Tobias.Turi <t.turi@jung.de>
2018-01-12 11:48:58 +0100

    remove 2 test options

--- Test/main_test.sh
+++ Test/main_test.sh
@@ -259,23 +259,11 @@ head_trace "$DOWNLOAD_TEXT"
 
 checkstep start_download_test
 
-declare LOGFILE=log2.file
-
-head_trace "$DOWNLOAD_WITHOUT_PARAMETER_TEXT"
-
-checkstep start_download_test_without_parameter
-
 declare LOGFILE=log3.file
 
 head_trace "$COPY_TEXT"
 
 checkstep start_copy_test
-
-declare LOGFILE=log4.file
-
-head_trace "$COPY_WITHOUT_PARAMETER_TEXT"
-
-checkstep start_copy_test_without_parameter
 
 head_trace "Delete the Workspace"
 
